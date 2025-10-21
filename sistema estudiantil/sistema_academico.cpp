e57c57aef91b690a6dc516354a31dcf0f896ba27
cbenigi <cristian.benitez959@pascualbravo.edu.co>
2025-10-21 12:14:38 -0500

    Migracion a listas enlazadas para Entrega 3

--- sistema estudiantil/sistema_academico.cpp
+++ sistema estudiantil/sistema_academico.cpp
@@ -2,71 +2,184 @@
 
 using namespace std;
 
-const int MAX_EST = 100;
-const int MAX_MAT = 10;
+struct NodoMateria {
+    string nombreMateria;
+    float nota;
+    NodoMateria* siguiente;
+};
 
-int N, M;
-string estudiantes[MAX_EST];
-string codigos[MAX_EST];
-string materias[MAX_MAT];
-float notas[MAX_EST][MAX_MAT];
+struct NodoEstudiante {
+    string nombre;
+    string codigo;
+    NodoMateria* materias;
+    NodoEstudiante* siguiente;
+};
 
-float promedio_estudiante(int i) {
-    float suma = 0;
-    for (int j = 0; j < M; j++) suma += notas[i][j];
-    return (M > 0) ? suma / M : 0;
+NodoEstudiante* listaEstudiantes = nullptr;
+string* nombresMaterias = nullptr;
+int M = 0;
+
+void insertarMateria(NodoEstudiante* estudiante, string nombreMat, float nota) {
+    NodoMateria* nuevo = new NodoMateria;
+    nuevo->nombreMateria = nombreMat;
+    nuevo->nota = nota;
+    nuevo->siguiente = nullptr;
+    
+    if (estudiante->materias == nullptr) {
+        estudiante->materias = nuevo;
+    } else {
+        NodoMateria* temp = estudiante->materias;
+        while (temp->siguiente != nullptr) {
+            temp = temp->siguiente;
+        }
+        temp->siguiente = nuevo;
+    }
 }
 
-float promedio_materia(int j) {
+void insertarEstudiante(string nombre, string codigo) {
+    NodoEstudiante* nuevo = new NodoEstudiante;
+    nuevo->nombre = nombre;
+    nuevo->codigo = codigo;
+    nuevo->materias = nullptr;
+    nuevo->siguiente = nullptr;
+    
+    if (listaEstudiantes == nullptr) {
+        listaEstudiantes = nuevo;
+    } else {
+        NodoEstudiante* temp = listaEstudiantes;
+        while (temp->siguiente != nullptr) {
+            temp = temp->siguiente;
+        }
+        temp->siguiente = nuevo;
+    }
+}
+
+float promedioEstudiante(NodoEstudiante* estudiante) {
     float suma = 0;
-    for (int i = 0; i < N; i++) suma += notas[i][j];
-    return (N > 0) ? suma / N : 0;
+    int count = 0;
+    NodoMateria* temp = estudiante->materias;
+    
+    while (temp != nullptr) {
+        suma += temp->nota;
+        count++;
+        temp = temp->siguiente;
+    }
+    
+    return (count > 0) ? suma / count : 0;
+}
+
+float promedioMateria(string nombreMat) {
+    float suma = 0;
+    int count = 0;
+    NodoEstudiante* tempEst = listaEstudiantes;
+    
+    while (tempEst != nullptr) {
+        NodoMateria* tempMat = tempEst->materias;
+        while (tempMat != nullptr) {
+            if (tempMat->nombreMateria == nombreMat) {
+                suma += tempMat->nota;
+                count++;
+                break;
+            }
+            tempMat = tempMat->siguiente;
+        }
+        tempEst = tempEst->siguiente;
+    }
+    
+    return (count > 0) ? suma / count : 0;
+}
+
+void mostrarEstudiantes() {
+    cout << "\n--- Listado ---\n";
+    NodoEstudiante* temp = listaEstudiantes;
+    
+    while (temp != nullptr) {
+        cout << temp->nombre << " (" << temp->codigo << ")\n";
+        
+        NodoMateria* tempMat = temp->materias;
+        while (tempMat != nullptr) {
+            cout << "   " << tempMat->nombreMateria << ": " << tempMat->nota << "\n";
+            tempMat = tempMat->siguiente;
+        }
+        
+        cout << "   Promedio: " << promedioEstudiante(temp) << "\n";
+        temp = temp->siguiente;
+    }
+}
+
+void mostrarEstadisticas() {
+    cout << "\n--- Estadisticas ---\n";
+    for (int i = 0; i < M; i++) {
+        cout << nombresMaterias[i] << " -> Promedio: " << promedioMateria(nombresMaterias[i]) << "\n";
+    }
+}
+
+void liberarMemoria() {
+    NodoEstudiante* tempEst = listaEstudiantes;
+    
+    while (tempEst != nullptr) {
+        NodoMateria* tempMat = tempEst->materias;
+        while (tempMat != nullptr) {
+            NodoMateria* borrarMat = tempMat;
+            tempMat = tempMat->siguiente;
+            delete borrarMat;
+        }
+        
+        NodoEstudiante* borrarEst = tempEst;
+        tempEst = tempEst->siguiente;
+        delete borrarEst;
+    }
+    
+    delete[] nombresMaterias;
 }
 
 int main() {
+    int N;
+    
     cout << "=== Sistema Academico Basico ===\n";
     cout << "Numero de estudiantes: ";
     cin >> N;
     cout << "Numero de materias: ";
     cin >> M;
 
-    cin.ignore(); 
+    cin.ignore();
 
+    nombresMaterias = new string[M];
     
     cout << "\n--- Ingreso de materias ---\n";
     for (int j = 0; j < M; j++) {
         cout << "Materia " << j + 1 << ": ";
-        getline(cin, materias[j]);
+        getline(cin, nombresMaterias[j]);
     }
 
     cout << "\n--- Ingreso de estudiantes ---\n";
     for (int i = 0; i < N; i++) {
+        string nombre, codigo;
+        
         cout << "Nombre estudiante " << i + 1 << ": ";
-        getline(cin, estudiantes[i]);
+        getline(cin, nombre);
         cout << "Codigo: ";
-        getline(cin, codigos[i]);
+        getline(cin, codigo);
+
+        insertarEstudiante(nombre, codigo);
+        
+        NodoEstudiante* temp = listaEstudiantes;
+        while (temp->siguiente != nullptr) {
+            temp = temp->siguiente;
+        }
 
         for (int j = 0; j < M; j++) {
-            cout << "Nota en " << materias[j] << ": ";
-            cin >> notas[i][j];
+            float nota;
+            cout << "Nota en " << nombresMaterias[j] << ": ";
+            cin >> nota;
+            insertarMateria(temp, nombresMaterias[j], nota);
         }
         cin.ignore();
     }
 
-    cout << "\n--- Listado ---\n";
-    for (int i = 0; i < N; i++) {
-        cout << estudiantes[i] << " (" << codigos[i] << ")\n";
-        for (int j = 0; j < M; j++) {
-            cout << "   " << materias[j] << ": " << notas[i][j] << "\n";
-        }
-        cout << "   Promedio: " << promedio_estudiante(i) << "\n";
-    }
-
-    cout << "\n--- Estadisticas ---\n";
-    for (int j = 0; j < M; j++) {
-        cout << materias[j] << " -> Promedio: " << promedio_materia(j) << "\n";
-    }
+    mostrarEstudiantes();
+    mostrarEstadisticas();
+    liberarMemoria();
 
     return 0;
 }
-
